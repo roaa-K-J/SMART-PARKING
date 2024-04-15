30b5fc7cad1597c1a1dbd08a3fa7260143aa131a
roaa-K-J <167127833+roaa-K-J@users.noreply.github.com>
2024-04-15 22:31:20 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Feedback from './component/Feedback';
+import Home from './component/Home';
+import Contact from './component/Contact';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+       <div>
+        <Navbar/>
+        <Routes>
+        <Route path ="/" element={<Home/>}/>
+        <Route path ="/contact" element={<Contact/>}/>
+        <Route path ="/feedback" element={<Feedback/>}/>
+        
+        </Routes>
+        <Footer/>
+      </div>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
