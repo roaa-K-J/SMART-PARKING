648b411a5e9b0ea0dbf3a0c8d32194b5829c4bd1
roaa-K-J <167127833+roaa-K-J@users.noreply.github.com>
2024-04-18 16:18:12 +0300

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,35 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../image/logo2.png';
-import '../style/Nav.css';
+
 const Navbar = () => {
   return (
-    <div>
-       <nav>
-            <ul>
-                <li><img className="imo" src={logo} alt="logo.png" /></li>
-                <li>
-                  <Link to="/">Home</Link>
-                  </li>
-                <li>
-                   <Link to="/ViewParking">Parking Spaces</Link>
-                   </li>
-                <li>
-                  <Link to="/about">About Us</Link>
-                  </li>
-                <li>
-                  <Link to="/feedback">Feedback</Link>
-                  </li>
-                <li> 
-               
-                  <Link to="/contact">Contact Us</Link>
-                  </li>
-            </ul>
-
-        </nav>
-       
-    </div>
-
+    <>
+      {/* start of header */}
+      <nav className="navbar ms-5 navbar-expand-lg bg-transparent">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img src={logo} alt="Bootstrap" width="100" />
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link" to="/Home">Home</Link>
+              <Link className="nav-link" to="/ParkingSpaces">Parking Spaces</Link>
+              <Link className="nav-link" to="/Feedback">Feedback</Link>
+              <Link className="nav-link" to="/Contact">Contact Us</Link>
+            </div>
+          </div>
+          <nav className="navbar bg-bg-transparent">
+            <div className="container-fluid">
+              <form className="d-flex ms-auto me-4" role="search">
+                <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+              </form>
+            </div>
+          </nav>
+        </div>
+      </nav>
+      {/* end of header */}
+    </>
   );
 };
 
